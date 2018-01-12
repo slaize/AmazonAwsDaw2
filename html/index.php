99a43672b9da11a81eed3f64a9d88c06e56bed72
Slaiz3 <slaize@gmail.com>
2018-01-12 23:14:45 +0200

    test git delete

--- html/index.php
+++ html/index.php
@@ -4,9 +4,6 @@
 </head>
 <body>
 <h1>Mi primera pagina en Amazon</h1>
-<h1>Mi primera pagina en Amazon</h1>
-<h1>Mi primera pagina en Amazon</h1>
-<h1>Mi primera pagina en Amazon</h1>
 <p>Esto es una prueba</p>
 </body>
 
