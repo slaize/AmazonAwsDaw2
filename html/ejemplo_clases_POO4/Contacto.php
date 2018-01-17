c27edd909def7584ea59b995d6115a411ba5dd78
Slaiz3 <slaize@gmail.com>
2018-01-17 16:14:51 +0100

    añadido ejemplo 4

---
+++ html/ejemplo_clases_POO4/Contacto.php
@@ -0,0 +1,21 @@
+<?php
+class Contacto{
+    // Variables o atributos
+    var $nombre;
+    var $apellidos;
+    var $edad;
+    var $email;
+
+    //Metodo constructor nuevo
+    function __construct($miNombre,$misApellidos,$miEdad,$miEmail){
+        $this->nombre = $miNombre;
+        $this->apellidos = $misApellidos;
+        $this->edad = $miEdad;
+        $this->email = $miEmail;
+    }
+
+    function setEmail($miEmail){
+        $this->email = $miEmail;
+    }
+
+}
